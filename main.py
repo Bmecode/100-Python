8fa1a0a7d5946e3e1fe6330ebfc6797caa6b4975
Burak <burakmert2697@gmail.com>
2024-11-18 00:20:51 +0300

    day3 exercise2

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 
 
 
+#DAY3 EXERCISE 2
+""" total=0
+print("Welcome Python Pizza Deliveries!")
+size=input("What size pizza do you want? S, M or L: ")
+if size=="S" or size=="s":
+    total+=15
+elif size=="M" or size=="m":
+    total+=20
+elif size=="L" or size=="l":
+    total+=25
+else:
+    print("see you")
+pepporoni= input("Do you want pepperoni on your pizza? Y or N: ")
+if pepporoni=="Y" or pepporoni=="y":
+    total+=2
+extra_cheese= input("Do you want extra cheese? Y or N: ")
+if extra_cheese=="Y" or extra_cheese=="y":
+    total+=1
+print(f"Your final bill is: {total}₺")"""
 
+
+#DAY3 EXERCISE
+""" weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+if bmi<18.5:
+    print("Underweight")
+elif 18.5<=bmi<25:
+    print("Normal")
+elif 25<=bmi :
+    print("Overweight")
+ """
 
 
 #DAY2
@@ -14,6 +47,7 @@ tip_result= bill*total_tip
 total_bill=bill+tip_result
 bill_person=round(total_bill/people,2)
 print(f"Each person should pay: ₺{bill_person}")"""
+
 
 #DAY1
 """ print("Welcome to the Band Name Generator.")
