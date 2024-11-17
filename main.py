2f17187d6aed85c14c28abc1901b8c538c871a9a
Burak <burakmert2697@gmail.com>
2024-11-17 23:17:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+#DAY2
+"""print("Welcome the tip calculator!")
+bill=float(input("What was the total bill? ₺"))
+tip=int(input("How much tip would you like to give? 10, 12, or 15?"))#inte çevir
+people=int(input("How many people to split the bill?"))#inte çevir
+total_tip=round(tip/100,2)
+tip_result= bill*total_tip
+total_bill=bill+tip_result
+bill_person=round(total_bill/people,2)
+print(f"Each person should pay: ₺{bill_person}")"""
+
+#DAY1
+""" print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet) """
