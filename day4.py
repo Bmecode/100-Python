0e32c03f806e9b1a6ad092a2349b2dcb761cab56
Burak <burakmert2697@gmail.com>
2024-11-20 19:38:42 +0300

    +

--- day4.py
+++ day4.py
@@ -25,6 +25,30 @@ scissors = """
       (____)
 ---.__(___)
 """
+game_images = [rock, paper, scissors]
+
+user_choose = int(
+    input("What do you choose? Type 0 for Rock, 1 Paper or 2 for Scissors.")
+)
+if user_choose >= 0 and user_choose <= 2:
+    print(f" Your Chose\n{game_images[user_choose]}")
+    computer_choose = random.randint(0, 2)
+    print(f"Computer Chose\n{game_images[computer_choose]}")
+if user_choose >= 3 or user_choose < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choose == 0 and computer_choose == 2:
+    print("You Win")
+elif computer_choose == 0 and user_choose == 2:
+    print("You Lose")
+elif computer_choose > user_choose:
+    print("You Lose")
+elif user_choose > computer_choose:
+    print("You Win")
+elif computer_choose == user_choose:
+    print("It's draw!")
+
+# benim çözümüm
+""" 
 #girdi alma
 user_choose = int(
     input("What do you choose? Type 0 for Rock, 1 Paper or 2 for Scissors.")
@@ -74,7 +98,7 @@ elif user_choose == 2:
         print("Draw")
 else:
     print("Wrong input!")
-
+ """
 
 # DAY4 EXERCISE2
 """ import random
